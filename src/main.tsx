ff8c6d2feb999c5382e9dc061bd8ffc8b97a5b27
mark <mark@totoro2-2.local>
2024-11-27 17:34:43 -0800

    make pwa, fix lint for web serial, web usb

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { registerSW } from "virtual:pwa-register";
+
+// add this to prompt for a refresh
+const updateSW = registerSW({
+    onNeedRefresh() {
+        if (confirm("New content available. Reload?")) {
+            updateSW(true);
+        }
+    },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+      VitePWA({
+          // cache all imports
+          workbox: {
+              globPatterns: ["**/*"],
+          },
+          // cache all assets public
+          includeAssets: [
+              "**/*",
+          ],
+          manifest: {
+              "name": "mark's fit test console",
+              "icons": [
+                  {
+                      "src": "icons/mftc-icon-192.png",
+                      "type": "image/png",
+                      "sizes": "192x192"
+                  }
+              ],
+              "start_url": "/fit-test-console",
+              "display": "standalone"
+          }
+      }),
+  ],
   base: "/fit-test-console",
   define: {
     __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
