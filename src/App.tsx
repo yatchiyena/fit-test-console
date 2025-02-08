fbdfddd19b9aca1e8d13bbd48427b1da0c48deaa
yatchi <>
2025-02-07 21:08:59 -0800

    Fix verbose speech

--- src/App.tsx
+++ src/App.tsx
@@ -407,6 +407,9 @@ function App() {
         dataCollectorStates.sayParticleCount = sayParticleCount;
     }, [sayParticleCount, dataCollectorStates]);
     useEffect(() => {
+        dataCollectorStates.verboseSpeech = verboseSpeech;
+    }, [verboseSpeech, dataCollectorStates]);
+    useEffect(() => {
         dataCollectorStates.sayEstimatedFitFactor = sayEstimatedFitFactor;
     }, [sayEstimatedFitFactor, dataCollectorStates]);
     useEffect(() => {
